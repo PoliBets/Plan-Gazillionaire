d924c71ef86eb66e56e25b143a428df0adbe84ec
Allan Zhan <allanzhan@allans-air.mynetworksettings.com>
2024-12-04 19:01:33 -0500

    updated requirements.txt

--- similar_events.py
+++
@@ -1,6 +0,0 @@
-# Deleted everything
-
-# Will not use this file anymore
-
-# If you want to see similar events -> go main.py, run the program 
-# and click on "5. Manage Similar Events" option to view and alter similar events
